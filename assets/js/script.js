0690e6827c93cd498abd0b5a738801e7ec20253e
Deepakvijayan07 <deepakvijayan31@gmail.com>
2025-12-26 11:14:53 +0530

    Auto-generated commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,18 @@
 $(document).ready(function () {
 
 
-
-
-  window.addEventListener('scroll', function () {
-    const header = document.querySelector('#top_header');
-    const scrollPosition = window.scrollY;
-    const hideThreshold = 80; // Adjust this value to hide the element after scrolling 200px
-
-    if (scrollPosition > hideThreshold) {
-      $('#top_header').slideUp(450);
-      // $('.navbar-custom').addClass('fixed-header-class')
-    } else {
-      $('#top_header').slideDown(450);
-      // $('.navbar-custom').removeClass('fixed-header-class')
-    }
+  var height=$('#top_header').height();
+  $(window).scroll(function(){
+      if ($(window).scrollTop() >= height) {
+      $('.fixed-logo').addClass('fixed-header-class')
+      $('.navbar-custom ').addClass('fixed-header-class')
+      }
+      else {
+      $('.fixed-logo').removeClass('fixed-header-class')
+      $('.navbar-custom ').removeClass('fixed-header-class');
+      }
   });
 
+
 });
+
